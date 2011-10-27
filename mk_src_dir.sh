449f30f7bec0d1cce0eb2a0f7dc4f7da9a264574
Antony Pavlov <antonynpavlov@gmail.com>
2011-10-27 19:50:13 +0400

    pc, versatile, ss20: use linux-3.1
    Signed-off-by: Antony Pavlov <antonynpavlov@gmail.com>

--- mk_src_dir.sh
+++ mk_src_dir.sh
@@ -9,6 +9,7 @@ mkdir -p $SRC_DIR
 ( cd $SRC_DIR; wget -c http://matt.ucc.asn.au/dropbear/releases/dropbear-0.53.1.tar.bz2 )
 ( cd $SRC_DIR; wget -c http://horms.net/projects/kexec/kexec-tools/kexec-tools-2.0.2.tar.gz )
 ( cd $SRC_DIR; wget -c http://barebox.org/download/barebox-2011.09.0.tar.bz2 )
+( cd $SRC_DIR; wget -c http://www.kernel.org/pub/linux/kernel/v3.x/linux-3.1.tar.xz )
 
 # mips barebox
 ( cd $SRC_DIR; wget -c http://prizma.bmstu.ru/~antony/barebox/barebox-2011.08.0-next.tar.bz2 )
